089c871b9d3f622f771e616df79d6d211d9537f4
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-09-14 11:48:54 +0330

    added layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+
+
+interface LayoutProps {
+    children:React.ReactNode
+}
+
+const Layout = ({children}:LayoutProps) => {
+  return (
+    <>
+    <header className="header">
+        <h2>project crm</h2>
+        <Link href={"/add-customer"}>Add Customer</Link>
+    </header>
+    <main className="main">
+        {children}
+    </main>
+
+    <footer className="footer">
+     <a>crm project</a> next.js
+    </footer>
+
+    </>
+  )
+}
+
+export default Layout
