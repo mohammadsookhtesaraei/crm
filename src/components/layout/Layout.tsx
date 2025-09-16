587bec97eaef3cd0206a1f38a1a93571e92339aa
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-09-16 11:33:35 +0330

    added mongoose create models and customer api route

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,27 +1,23 @@
-import Link from "next/link"
-
+import Link from "next/link";
 
 interface LayoutProps {
-    children:React.ReactNode
+  children: React.ReactNode;
 }
 
-const Layout = ({children}:LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-    <header className="header">
+      <header className="header">
         <h2>project crm</h2>
         <Link href={"/add-customer"}>Add Customer</Link>
-    </header>
-    <main className="main">
-        {children}
-    </main>
+      </header>
+      <main className="main">{children}</main>
 
-    <footer className="footer">
-     <a>crm project</a> next.js
-    </footer>
-
+      <footer className="footer">
+        <a>crm project</a> next.js
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
---
+++ src/pages/api/customer/index.ts
@@ -0,0 +1,57 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    // check connect db
+    try {
+        await connectDB();
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "Failed",
+            message: "Error in connecting to DB"
+        });
+        return
+    }
+
+    // check req post
+
+    if (req.method === "POST") {
+
+        // get data from request.body = client
+        const data = req.body.data;
+
+        // validation
+        if (!data.name || !data.lastName || !data.email) {
+            return res.status(400).json({
+                status: "Failed",
+                message: "Invalid Data!"
+            })
+        }
+
+        //    call customer model and create collaboration
+        try {
+            const customer = await Customer.create(data);
+            res.status(201).json({
+                status: "success",
+                message: "Data created",
+                data: customer
+            })
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                status: "Failed",
+                message: "Error in storing data in DB"
+            })
+        }
+    }
+
+
+
+}
